e6c8c24411c9716b5affde390d8b250cd87db9cb
JEPTANUI <milliemutaijie978@gmail.com>
2023-06-15 08:54:23 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main- A progam that prints puts function
+ *return(0)- output success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
