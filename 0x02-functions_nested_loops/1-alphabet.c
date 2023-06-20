75a6609e7ff38700bb3ed43316c1cb9d56f855ac
JEPTANUI <milliemutaijie978@gmail.com>
2023-06-20 09:00:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 56; i <= 93; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
