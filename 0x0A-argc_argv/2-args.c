26a071b3a29336d6731cebd0af9d39e48b74ab16
JEPTANUI <milliemutaijie978@gmail.com>
2023-07-12 11:38:54 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,10 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+int count = 0;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+if (argc > 0)
+{
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
 }
